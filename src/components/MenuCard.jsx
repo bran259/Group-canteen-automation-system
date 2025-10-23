0d35252b4ea5b3fda024885025eba48327be7e29
bran259 <kiprop686@gmail.com>
2025-10-24 00:03:41 +0300

    change menucarts

--- src/components/MenuCard.jsx
+++ src/components/MenuCard.jsx
@@ -1,21 +1,3 @@
-// import React from "react";
-
-
-// export default function MenuCard({ item, addToCart }) {
-//   return (
-//     <div className="border p-4 rounded shadow bg-white">
-//       <h2 className="font-bold text-lg">{item.name}</h2>
-//       <p>{item.description}</p>
-//       <p className="text-green-600">₱{item.price}</p>
-//       <button
-//         onClick={() => addToCart(item)}
-//         className="mt-2 bg-blue-500 text-white px-3 py-1 rounded"
-//       >
-//         Add to Cart
-//       </button>
-//     </div>
-//   );
-// }
 import React from "react";
 import "./MenuCard.css";
 
