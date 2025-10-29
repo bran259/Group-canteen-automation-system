be6e656712385892f0e8f2d45e614c973feb5dd6
bran259 <kiprop686@gmail.com>
2025-10-29 16:45:23 +0300

    modified and added changes to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,76 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import NavBar from "./components/NavBar";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Menu from "./pages/Menu";
-import Orders from "./pages/Orders";
-import AdminPanel from "./pages/AdminPanel";
-import ProtectedRoute from "./components/ProtectedRoute";
+import React from 'react';
+import { AppProvider, useAppContext } from './context/AppContext';
+import Navigation from './components/Navigation';
+import Notification from './components/Notification';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import MenuPage from './pages/MenuPage';
+import CartPage from './pages/CartPage';
+import PaymentPage from './pages/PaymentPage';
+import OrdersPage from './pages/OrdersPage';
+import ReceiptPage from './pages/ReceiptPage';
+import AdminDashboard from './pages/admin/AdminDashboard';
+import AdminMenuPage from './pages/admin/AdminMenuPage';
+import AdminOrdersPage from './pages/admin/AdminOrdersPage';
+import AdminInventoryPage from './pages/admin/AdminInventoryPage';
+import AdminReportsPage from './pages/admin/AdminReportsPage';
+import AdminUsersPage from './pages/admin/AdminUsersPage';
 
-function App() {
+const AppContent = () => {
+  const { currentUser, currentPage, notification } = useAppContext();
+
+  const renderPage = () => {
+    if (!currentUser) {
+      if (currentPage === 'register') return <RegisterPage />;
+      return <LoginPage />;
+    }
+
+    if (currentPage.startsWith('receipt-')) {
+      const orderId = currentPage.split('-')[1];
+      return <ReceiptPage orderId={orderId} />;
+    }
+
+    switch (currentPage) {
+      case 'menu':
+        return <MenuPage />;
+      case 'cart':
+        return <CartPage />;
+      case 'payment':
+        return <PaymentPage />;
+      case 'orders':
+        return <OrdersPage />;
+      case 'admin-dashboard':
+        return <AdminDashboard />;
+      case 'admin-menu':
+        return <AdminMenuPage />;
+      case 'admin-orders':
+        return <AdminOrdersPage />;
+      case 'admin-inventory':
+        return <AdminInventoryPage />;
+      case 'admin-reports':
+        return <AdminReportsPage />;
+      case 'admin-users':
+        return <AdminUsersPage />;
+      default:
+        return <MenuPage />;
+    }
+  };
+
   return (
-    <Router>
-      <NavBar />
-      <div className="p-6">
-        <Routes>
-          <Route path="/" element={<Menu />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route
-            path="/orders"
-            element={
-              <ProtectedRoute>
-                <Orders />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/admin"
-            element={
-              <ProtectedRoute role="admin">
-                <AdminPanel />
-              </ProtectedRoute>
-            }
-          />
-        </Routes>
-      </div>
-    </Router>
+    <div className="min-h-screen bg-gray-50">
+      {notification && <Notification message={notification.message} type={notification.type} />}
+      <Navigation />
+      {renderPage()}
+    </div>
   );
-}
+};
+
+const App = () => {
+  return (
+    <AppProvider>
+      <AppContent />
+    </AppProvider>
+  );
+};
 
 export default App;
